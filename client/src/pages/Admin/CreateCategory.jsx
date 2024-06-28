6bab24d5aabdf3217485dbfa3f660f3d6de11609
Nikith-S <nikith391@gmail.com>
2024-06-28 22:40:03 +0530

    commit

--- client/src/pages/Admin/CreateCategory.jsx
+++ client/src/pages/Admin/CreateCategory.jsx
@@ -4,34 +4,34 @@ import AdminMenu from '../../components/Layout/AdminMenu';
 import toast from 'react-hot-toast';
 import axios from 'axios';
 import CategoryForm from '../../components/Form/CategoryForm';
+import { Modal } from 'antd';
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
+  const [name, setName] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState('');
 
-  const [name, setName] = useState("");
-
-  const handleSubmit= async(e)=> {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      const {data} = await axios.post(`${process.env.REACT_APP_API}/api/v1/category/create-category`, {name})
-      if(data?.success){
-        toast.success(`${name} is created`)
+    try {
+      const { data } = await axios.post(`${process.env.REACT_APP_API}/api/v1/category/create-category`, { name });
+      if (data?.success) {
+        toast.success(`${name} is created`);
         getAllCategory();
+      } else {
+        toast.error(data.message);
       }
-      else{
-        toast.error(data.message)
-      }
-    }catch(error){
-      console.log(error)
-      toast.error("Something went wrong in Input form")
-
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong in Input form');
     }
-  }
+  };
 
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/category/getcategory`);
-
       if (data.success) {
         setCategories(data.category);
       }
@@ -45,6 +45,17 @@ const CreateCategory = () => {
     getAllCategory();
   }, []);
 
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+    
+    } catch (error) {
+      console.log(error);
+      toast.error('There is some problem in update function');
+    }
+  };
+
+
   return (
     <Layout title={'DashBoard - Create Product'}>
       <div className="container-fluid m-3 p-3">
@@ -55,11 +66,9 @@ const CreateCategory = () => {
           <div className="col-md-9">
             <h1>Manage Category</h1>
             <div className="p-3 w-50">
-            <CategoryForm handleSubmit={handleSubmit} value={name} setValue={setName}/>
-
+              <CategoryForm handleSubmit={handleSubmit} value={name} setValue={setName} />
             </div>
             <div className="w-75">
-            <div>
               <table className="table">
                 <thead>
                   <tr>
@@ -72,16 +81,17 @@ const CreateCategory = () => {
                     <tr key={c._id}>
                       <td>{c.name}</td>
                       <td>
-                        <button className="btn btn-primary ms-2">Edit</button>
+                        <button className="btn btn-primary ms-2" onClick={() => { setVisible(true); setUpdatedName(c.name) }}>Edit</button>
                         <button className="btn btn-danger ms-2">Delete</button>
-
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
-            </div>
+            <Modal onCancel={() => setVisible(false)} visible={visible} footer={null}>
+              <CategoryForm value={updatedName} setValue={setUpdatedName} handleSubmit={handleUpdate} />
+            </Modal>
           </div>
         </div>
       </div>
