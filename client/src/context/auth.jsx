f8f5717aa6f64dbc19f97dd728a42f8d493c4ef3
Nikith-S <nikith391@gmail.com>
2024-06-30 19:34:41 +0530

    commit

--- client/src/context/auth.jsx
+++ client/src/context/auth.jsx
@@ -1,6 +1,12 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+
+
+
+
 const AuthContext = createContext();
+
+
 
 
 export const AuthProvider = ({ children }) => {
@@ -8,14 +14,9 @@ export const AuthProvider = ({ children }) => {
     user: null,
     token: ''
   });
-
-
   // default axios
 axios.defaults.headers.common['Authorization']= auth?.token
-
-
   useEffect(() => {
-
 const data = localStorage.getItem('auth')
 if(data){
     const paraseData = JSON.parse(data)
